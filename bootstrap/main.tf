2e5916114da78a09d658879dc1f12f4bae549706
Alessandro Grassi <alessandrograssi17@gmail.com>
2024-10-08 16:28:56 +0000

    feat: add bootstrap

---
+++ bootstrap/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.70.0"
+    }
+  }
+}
+
+
+variable "aws_profile" {
+  description = "AWS profile to use"
+  type        = string
+}
+
+locals {
+  aws_region  = "eu-west-1"
+  aws_profile = var.aws_profile
+
+  app   = "imsub"
+  stack = "bootstrap"
+}
+
+provider "aws" {
+  region = local.aws_region
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "${local.app}-terraform-state"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state" {
+  name         = "${local.app}-terraform-state"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
